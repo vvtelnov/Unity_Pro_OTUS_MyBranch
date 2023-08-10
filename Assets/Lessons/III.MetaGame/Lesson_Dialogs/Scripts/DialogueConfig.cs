50af0201c18882c8899fd7429c9f22f98359bb83
starkre <gulkin.igor.developer@gmail.com>
2023-08-10 17:09:58 +0600

    Материалы готовы!

--- Assets/Lessons/III.MetaGame/Lesson_Dialogs/Scripts/DialogueConfig.cs
+++ Assets/Lessons/III.MetaGame/Lesson_Dialogs/Scripts/DialogueConfig.cs
@@ -12,5 +12,45 @@ namespace Lessons.MetaGame.Dialogs
         public string startNodeId;
         public List<DialogueNodeSerialized> nodes;
         public List<DialogueEdgeSerialzied> edges;
+        
+        public bool FindStartNode(out DialogueNodeSerialized node)
+        {
+            return this.FindNode(this.startNodeId, out node);
+        }
+
+        private bool FindNode(string id, out DialogueNodeSerialized result)
+        {
+            foreach (var node in this.nodes)
+            {
+                if (node.id == id)
+                {
+                    result = node;
+                    return true;
+                }
+            }
+        
+            result = default;
+            return false;
+        }
+
+        public bool FindNextNode(string sourceNodeId, int choiceIndex, out DialogueNodeSerialized nextNode)
+        {
+            for (int i = 0, count = this.edges.Count; i < count; i++)
+            {
+                var edge = this.edges[i];
+                if (edge.sourceNodeId == sourceNodeId && edge.index == choiceIndex)
+                {
+                    if (this.FindNode(edge.targetNodeId, out nextNode))
+                    {
+                        return true;
+                    }
+        
+                    return false;
+                }
+            }
+
+            nextNode = default;
+            return false;
+        }
     }
 }
